93cf1ca7830b6f76006a15c72c560a9a8d032a10
ravveni <ravveni420@gmail.com>
2024-03-01 14:39:50 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module quick-server
+
+go 1.21.7
+
+require github.com/russross/blackfriday v1.6.0
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/russross/blackfriday"
+)
+
+func renderIndex(w http.ResponseWriter, r *http.Request){
+	markdown, err := os.ReadFile("./index.md")
+	if err != nil {
+	    log.Fatal(err)
+	}
+
+    html := blackfriday.MarkdownBasic(markdown)
+
+    err = os.WriteFile("index.html", html, os.ModePerm)
+	if err != nil {
+	   log.Fatal(err)
+	}
+
+     http.ServeFile(w, r, "./index.html")
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./share"))
+
+	http.HandleFunc("/", renderIndex)
+	http.Handle("/share/", http.StripPrefix("/share/",fs))
+
+	log.Println("Listening on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
