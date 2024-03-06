c9ffc9ae7712636efe229ebc0feac45d25b40089
ravveni <ravveni420@gmail.com>
2024-03-06 15:01:04 -0500

    updated to blackfriday v2, port selection as command argument

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ravveni/wi-fileshare
 
 go 1.21.7
 
-require github.com/russross/blackfriday v1.6.0
+require github.com/russross/blackfriday/v2 v2.1.0
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
-	"github.com/russross/blackfriday"
+	"github.com/russross/blackfriday/v2"
 )
+
+const defaultPort = 8080
 
 func renderIndex(w http.ResponseWriter, r *http.Request) {
 	markdown, err := os.ReadFile("./index.md")
@@ -14,7 +18,7 @@ func renderIndex(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	html := blackfriday.MarkdownBasic(markdown)
+	html := blackfriday.Run(markdown)
 
 	err = os.WriteFile("index.html", html, os.ModePerm)
 	if err != nil {
@@ -24,15 +28,43 @@ func renderIndex(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./index.html")
 }
 
-func main() {
+func startServer(port int) {
+	serverPort := fmt.Sprint(":",port)
 	fs := http.FileServer(http.Dir("./share"))
 
 	http.HandleFunc("/", renderIndex)
 	http.Handle("/share/", http.StripPrefix("/share/", fs))
 
-	log.Println("Listening on :8080")
-	err := http.ListenAndServe(":8080", nil)
+	log.Println("Listening on ", serverPort)
+	err := http.ListenAndServe(serverPort, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func validateInput(input string) (int, error) {
+	num, err := strconv.Atoi(input)
+	if err != nil {
+        return -1, fmt.Errorf("Invalid number: %v", err)
+	}
+
+	if num < 0 || num > 65536 {
+		return -1, fmt.Errorf("%d is not between 0 and 65536", num)
+	}
+
+   return num, nil
+}
+
+func main() {
+	port := defaultPort
+	args := os.Args[1:]
+	if len(args) > 0 {
+		validatedPort, err := validateInput(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		port = validatedPort
+	}
+
+	startServer(port)
 }
