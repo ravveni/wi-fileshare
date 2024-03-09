0db0c3201782f132a21665eb03aaf41d5158ab92
ravveni <ravveni420@gmail.com>
2024-03-08 22:21:36 -0500

    changed build naming style

--- build-release.sh
+++ build-release.sh
@@ -3,12 +3,12 @@
 BINARY_NAME="wi-fileshare"
 
 # linux
-GOOS=linux GOARCH=amd64 go build -o bin/${BINARY_NAME}-amd64-linux
-GOOS=linux GOARCH=arm64 go build -o bin/${BINARY_NAME}-arm64-linux
+GOOS=linux GOARCH=amd64 go build -o bin/linux-amd64-${BINARY_NAME}
+GOOS=linux GOARCH=arm64 go build -o bin/linux-arm64-${BINARY_NAME}
 
 # mac
-GOOS=darwin GOARCH=amd64 go build -o bin/${BINARY_NAME}-amd64-darwin
-GOOS=darwin GOARCH=arm64 go build -o bin/${BINARY_NAME}-arm64-darwin
+GOOS=darwin GOARCH=amd64 go build -o bin/darwin-amd64-${BINARY_NAME}
+GOOS=darwin GOARCH=arm64 go build -o bin/darwin-arm64-${BINARY_NAME}
 
 # windows
-GOOS=windows GOARCH=amd64 go build -o bin/${BINARY_NAME}-amd64.exe
+GOOS=windows GOARCH=amd64 go build -o bin/windows-amd64-${BINARY_NAME}.exe
